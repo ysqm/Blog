b3a82746a832f300610ada623f8705b8b6f49cb0
ysqm <411321639@qq.com>
2024-06-18 15:25:54 +0800

    environment

---
+++ elm-pojo/src/main/java/com/elm/dto/UserLoginDTO.java
@@ -0,0 +1,22 @@
+package com.elm.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * C端用户登录
+ */
+@Data
+@ApiModel(description = "客户登录时传递的数据模型")
+public class UserLoginDTO implements Serializable {
+
+    @ApiModelProperty("用户名")
+    private String username;
+
+    @ApiModelProperty("密码")
+    private String password;
+
+}
