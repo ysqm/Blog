cdb14ce07f4ae0c3dfc8cba454d549b91b854cd0
ysqm <411321639@qq.com>
2024-06-26 17:20:14 +0800

    front

--- elm-server/src/main/resources/data.sql
+++ elm-server/src/main/resources/data.sql
@@ -1,16 +1,18 @@
 CREATE DATABASE  IF NOT EXISTS `blogdata` ;
 USE `blogdata`;
 
-DROP TABLE IF EXISTS `Users`;
-CREATE TABLE Users (
-                       UserID           INT AUTO_INCREMENT PRIMARY KEY,
-                       Username         VARCHAR(50) UNIQUE NOT NULL,
-                       Password         VARCHAR(255) NOT NULL,
-                       Email            VARCHAR(100) UNIQUE NOT NULL,
-                       Avatar           TEXT,
-                       RegisterDate     DATETIME NOT NULL,
-                       Bio              TEXT,
-                       UpdateTime       DATETIME NOT NULL,
-                       WeChatAccount    VARCHAR(100),
-                       QQAccount        VARCHAR(100)
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE users (
+                       user_id           INT AUTO_INCREMENT PRIMARY KEY,
+                       username         VARCHAR(50) UNIQUE NOT NULL,
+                       password         VARCHAR(255) NOT NULL,
+                       email            VARCHAR(100) UNIQUE NOT NULL,
+                       avatar           TEXT,
+                       register_date     DATETIME NOT NULL,
+                        last_login_time     DATETIME NOT NULL,
+                       bio              TEXT,
+                       update_time       DATETIME NOT NULL,
+                       wechat_account    VARCHAR(100),
+                       qq_account        VARCHAR(100),
+                       is_logged_out    INT DEFAULT 0 CHECK (is_expired IN (0, 1)),
 );
