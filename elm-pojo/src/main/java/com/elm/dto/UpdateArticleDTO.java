d8ce4ce3587e2b6fd573f18bfa80d15b618b9682
EgoistSA <3046205147@qq.com>
2024-06-27 16:38:37 +0800

    初步实现文章管理功能

---
+++ elm-pojo/src/main/java/com/elm/dto/UpdateArticleDTO.java
@@ -0,0 +1,22 @@
+package com.elm.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.Serializable;
+
+@Data
+@ApiModel(description = "更新文章时传递的数据模型")
+public class UpdateArticleDTO implements Serializable {
+
+    @ApiModelProperty("文章标题")
+    private String title;
+
+    @ApiModelProperty("文章文件")
+    private MultipartFile file;
+
+    @ApiModelProperty("文章状态")
+    private String status;
+}
--- elm-server/src/main/resources/data.sql
+++ elm-server/src/main/resources/data.sql
@@ -3,14 +3,46 @@ USE `blogdata`;
 
 DROP TABLE IF EXISTS `Users`;
 CREATE TABLE Users (
-                       UserID           INT AUTO_INCREMENT PRIMARY KEY,
-                       Username         VARCHAR(50) UNIQUE NOT NULL,
-                       Password         VARCHAR(255) NOT NULL,
-                       Email            VARCHAR(100) UNIQUE NOT NULL,
-                       Avatar           TEXT,
-                       RegisterDate     DATETIME NOT NULL,
-                       Bio              TEXT,
-                       UpdateTime       DATETIME NOT NULL,
-                       WeChatAccount    VARCHAR(100),
-                       QQAccount        VARCHAR(100)
+                       user_id           INT AUTO_INCREMENT PRIMARY KEY,
+                       username         VARCHAR(50) UNIQUE NOT NULL,
+                       password         VARCHAR(255) NOT NULL,
+                       email            VARCHAR(100) ,
+                       avatar           TEXT,
+                       register_date     DATETIME,
+                       bio              TEXT,
+                       update_time       DATETIME,
+                       wechat_account    VARCHAR(100),
+                       qq_account        VARCHAR(100)
+);
+
+-- 创建文章表
+DROP TABLE IF EXISTS `Articles`;
+CREATE TABLE `Articles` (
+                            `article_id` INT AUTO_INCREMENT PRIMARY KEY,
+                            `user_id` INT NOT NULL,
+                            `title` VARCHAR(255) NOT NULL,
+                            `content_path` VARCHAR(255) NOT NULL, -- 文件路径
+                            `publish_date` DATETIME NOT NULL,
+                            `update_date` DATETIME,
+                            `status` ENUM('published', 'draft', 'deleted', 'hidden') NOT NULL,
+                            `heat` INT DEFAULT 0,
+                            `is_deleted` BOOLEAN DEFAULT 0,
+                            FOREIGN KEY (`user_id`) REFERENCES `Users`(`user_id`)
+);
+
+-- 创建标签表
+DROP TABLE IF EXISTS `Tags`;
+CREATE TABLE `Tags` (
+                        `tag_id` INT AUTO_INCREMENT PRIMARY KEY,
+                        `tag_name` VARCHAR(50) UNIQUE NOT NULL
+);
+
+-- 创建文章标签表
+DROP TABLE IF EXISTS `ArticleTags`;
+CREATE TABLE `ArticleTags` (
+                               `article_id` INT,
+                               `tag_id` INT,
+                               PRIMARY KEY (`article_id`, `tag_id`),
+                               FOREIGN KEY (`article_id`) REFERENCES `Articles`(`article_id`) ON DELETE CASCADE,
+                               FOREIGN KEY (`tag_id`) REFERENCES `Tags`(`tag_id`) ON DELETE CASCADE
 );
