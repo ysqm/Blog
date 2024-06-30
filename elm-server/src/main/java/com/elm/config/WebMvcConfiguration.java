2e595660a0aaf5caa15a5a9feb1674130ab340c0
ysqm <411321639@qq.com>
2024-06-30 22:58:46 +0800

    修复了返回userId为null的问题

--- elm-server/src/main/java/com/elm/config/WebMvcConfiguration.java
+++ elm-server/src/main/java/com/elm/config/WebMvcConfiguration.java
@@ -32,9 +32,10 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
      */
     protected void addInterceptors(InterceptorRegistry registry) {
         log.info("开始注册自定义拦截器...");
-//        registry.addInterceptor(jwtTokenAdminInterceptor)
-//                .addPathPatterns("/admin/**")
-//                .excludePathPatterns("/admin/employee/login");
+        registry.addInterceptor(jwtTokenAdminInterceptor)
+                .addPathPatterns("/admin/**")
+                .excludePathPatterns("/admin/user/login")
+                .excludePathPatterns("/admin/user/register");
     }
 
     /**
