e924678926eb8d6ebee33548804cf35e6cf5a0b9
ysqm <411321639@qq.com>
2024-07-01 10:44:20 +0800

    令牌验证用户id

--- elm-server/src/main/java/com/elm/config/WebMvcConfiguration.java
+++ elm-server/src/main/java/com/elm/config/WebMvcConfiguration.java
@@ -33,7 +33,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
     protected void addInterceptors(InterceptorRegistry registry) {
         log.info("开始注册自定义拦截器...");
         registry.addInterceptor(jwtTokenUserInterceptor)
-                .addPathPatterns("/user/**")
+                .addPathPatterns("/**")
                 .excludePathPatterns("/user/login")
                 .excludePathPatterns("/user/register");
     }
