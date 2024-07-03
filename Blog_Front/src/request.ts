8bc71d50c0159815d7fdc17e166ced2d1063ed36
ysqm <411321639@qq.com>
2024-07-03 15:57:54 +0800

    开启拦截器

--- Blog_Front/src/request.ts
+++ Blog_Front/src/request.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import store from './store/modules';
 
 export interface MyResponse{
     data:any;
@@ -8,7 +9,11 @@ export interface MyResponse{
 
 const instance = axios.create({
   // 可以在这里设置通用的请求配置，如baseURL、请求拦截器等
-    
+  headers: {
+    common: {
+      'token': store.state.token
+    }
+  }
 });
 
 export function request<T>(config: AxiosRequestConfig): Promise<AxiosResponse<T>> {
