cd7500c6f8126f22a4ffdc6821094a696ea2c190
Limerence <114558251+yeteye@users.noreply.github.com>
2024-07-04 08:47:56 +0800

    社区文章显示

--- elm-server/src/main/java/com/elm/service/impl/ArticleServiceImpl.java
+++ elm-server/src/main/java/com/elm/service/impl/ArticleServiceImpl.java
@@ -114,6 +114,11 @@ public class ArticleServiceImpl implements ArticleService {
         articleMapper.topArticle(articleId);
         return getArticleById(articleId);
     }
+    @Override
+    public List<ArticleVO> getLatestArticles() {
+        List<Article> articles = articleMapper.getLatestArticles();
+        return articles.stream().map(this::toVO).collect(Collectors.toList());
+    }
 
     private ArticleVO toVO(Article article) {
         ArticleVO vo = new ArticleVO();
@@ -182,5 +187,6 @@ public class ArticleServiceImpl implements ArticleService {
     private List<Long> getArticleTagIds(Long articleId) {
         return articleTagMapper.getTagIdsByArticleId(articleId);
     }
+
 }
 
