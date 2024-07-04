f30798fe0ea0c556909273124eb64296636d14e4
云岁月书 <69841501+ysqm@users.noreply.github.com>
2024-07-04 11:27:06 +0800

    Update request.ts

--- Blog_Front/src/request.ts
+++ Blog_Front/src/request.ts
@@ -9,12 +9,27 @@ export interface MyResponse{
 
 const instance = axios.create({
   // 可以在这里设置通用的请求配置，如baseURL、请求拦截器等
-  headers: {
-    common: {
-      'token': store.state.token
-    }
-  }
+  // headers: {
+  //   common: {
+  //     'token': store.state.token
+  //   }
+  // }
 });
+
+// 请求拦截器，在每次请求前，动态获取 store 中的 token 并添加到请求头
+instance.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers['token'] = ` ${token}`;
+        }
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+);
+
 
 export function request<T>(config: AxiosRequestConfig): Promise<AxiosResponse<T>> {
     return instance.request<T>(config);
